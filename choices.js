f40a2a35b21c202cb8675d649c45d7a48313e108
nikhilp <75940300+nikhilpabolu99@users.noreply.github.com>
2024-12-11 01:16:12 +0530

    Create choices.js

---
+++ choices.js
@@ -0,0 +1,2 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/choices.js/public/assets/styles/choices.min.css">
+<script src="https://cdn.jsdelivr.net/npm/choices.js/public/assets/scripts/choices.min.js"></script>
